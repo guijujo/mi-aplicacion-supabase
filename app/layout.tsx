f4301b5b83d3bf429a34bc3c24d372f46e22ba10
Guillermo Garcia <guijujo.25@gmail.com>
2024-05-22 10:15:42 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Talentos Digitales",
+  description: "Proyecto realizado en Talentos Digitales por Guillermo García",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es" className={GeistSans.className}>
+      <body className="bg-background text-foreground">
+        <main className="min-h-screen flex flex-col items-center">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
