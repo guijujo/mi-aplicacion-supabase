bae05403e39afb5cd7026e6696f7d04d25260ecd
Guillermo Garcia <guijujo.25@gmail.com>
2024-05-22 17:55:38 -0300

    Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -29,6 +29,7 @@ export default async function DashboardPage() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
+          <div>Productos</div>
           
         </main>
       </div>
